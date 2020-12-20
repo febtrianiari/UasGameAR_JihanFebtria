ceff665b051e1cf2ed9437e1c8774b5d5e9c71c6
febtrianiari <50358103+febtrianiari@users.noreply.github.com>
2020-12-20 21:18:44 +0700

    edukasibuah

---
+++ Assets/SistemGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SistemGame : MonoBehaviour
+{
+    public static SistemGame instance;
+    public int ID;
+    public GameObject TempatSpawn;
+    public GameObject[] KoleksiBuah;
+    
+
+    // Start is called before the first frame update
+    private void Awake() // diakses sebelum start dan update
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        SpawnObject();
+    }
+
+    public void SpawnObject()
+    {
+        GameObject Benda = Instantiate(KoleksiBuah[ID]);
+        
+        Benda.transform.SetParent(TempatSpawn.transform, false);
+        //Benda.transform.localScale = new Vector3(37f, 37f, 37f);
+    }
+}
